c20a264ac534210cdabe0f86ac662aebbaf7d0a6
creatorkotik <ivanmitraev8@gmail.com>
2024-02-17 13:56:49 +0300

    update

---
+++ script.js
@@ -0,0 +1,7 @@
+let img = document.querySelector(".img");
+let text = document.querySelector(".img__text");
+let minpage = document.querySelector(".minpage");
+let maxpage = document.querySelector(".maxpage");
+let btnleft = document.querySelector(".btn-left");
+let btnright = document.querySelector(".btn-right");
+let slides = document.querySelectorAll(".img__slide");
